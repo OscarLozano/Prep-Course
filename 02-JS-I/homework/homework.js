de5a3a9b31945d128d12630726c18d25a2fff06e
OscarLozano <olozano679@gmail.com>
2022-06-02 11:25:38 -0500

    completo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,8 +212,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * 1.2 
+  return euro * 1.2
 }
+
 
 function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
